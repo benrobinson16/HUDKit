41c9ccc8354ea11478d1e46ce9aeaf84abb13780
Ben Robinson <benrobinsonhk@gmail.com>
2021-02-25 21:28:24 +0000

    Basic implementation

--- README.md
+++ README.md
@@ -1,3 +1,78 @@
 # HUDKit
 
-A description of this package.
+A simple Swift package for managing Apple-like HUD views.
+
+## Basic Usage
+
+To use the default behaviour, first wrap your entire view hierachy in an `HUDContainer` like so (preferably in the `App.swift` file):
+
+```swift
+struct MyApp: Swift {
+    var body: some View {
+        WindowGroup {
+            HUDContainer {
+                ContentView()
+            }
+        }
+    }
+}
+```
+
+This will give the `HUDKit` API the ability to present an HUD above your view hierachy.
+
+Next, when you want to display a HUD:
+
+```swift
+HUDManager.shared.display("Hello, world!")
+
+// or, specifying a symbol
+
+HUDManager.shared.display("Hello, world!", .starFill)
+```
+
+The HUD will dismiss automatically after three seconds. To dismiss at any other time, call the following:
+
+```swift
+HUDManager.shared.dismiss()         // with animation
+HUDManager.shared.forceDismiss()    // without animation
+```
+
+## Advanced Usage
+
+If you wish to define your own HUD view content, you must make your own container and display manager. Please see the `Sources` directory for ideas on how to approach this.
+
+Simply, you can use the HUD in a `ZStack` with your custom content:
+
+```swift
+struct ContentView: View {
+    @State private var showHUD = false
+
+    var body: some View {
+        ZStack {
+            // your main view hierachy
+            if showHUD {
+                HUD {
+                    // your custom content
+                }
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                        withAnimation {
+                            manager.dismiss()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+```
+
+## Credits
+
+Inspired by the HUD interface in [this article](https://fivestars.blog/swiftui/swiftui-hud.html).
+
+[SFSafeSymbols](https://github.com/piknotech/SFSafeSymbols) is used to provide type-safe access to SFSymbols.
+
+## License
+
+Please see LICENSE.md.
--- Sources/HUDKit/HUDKit.swift
+++ Sources/HUDKit/HUDKit.swift
@@ -1,3 +1,117 @@
-struct HUDKit {
-    var text = "Hello, World!"
+// Copyright (c) 2021 Ben Robinson.
+// All Rights Reserved.
+
+import SwiftUI
+import SFSafeSymbols
+
+public struct HUD<Content: View>: View {
+    let content: () -> Content
+    
+    public init(content: @escaping () -> Content) {
+        self.content = content
+    }
+    
+    @available(macOS 11.0, *)
+    public init(_ title: String, symbol: SFSymbol? = nil) where Content == DefaultHUDContent {
+        self.init(content: { DefaultHUDContent(title, symbol: symbol) })
+    }
+    
+    public var body: some View {
+        content()
+            .padding(.horizontal, 12)
+            .padding(16)
+            .background(
+                Capsule()
+                    .foregroundColor(Color.white)
+                    .shadow(color: Color.black.opacity(0.16), radius: 12, x: 0, y: 5)
+            )
+            .transition(AnyTransition.move(edge: .top).combined(with: .opacity))
+            .zIndex(1)
+    }
+}
+
+@available(macOS 11.0, *)
+public struct DefaultHUDContent: View {
+    let title: String
+    let symbol: SFSymbol?
+    
+    public init(_ title: String, symbol: SFSymbol? = nil) {
+        self.title = title
+        self.symbol = symbol
+    }
+    
+    public var body: some View {
+        HStack(alignment: .center, spacing: 5.0) {
+            if let symbol = symbol {
+                Image(systemSymbol: symbol)
+            }
+            Text(title)
+        }
+    }
+}
+
+public struct HUDContainer<Content: View>: View {
+    @ObservedObject private var manager = HUDManager.shared
+    @State private var id: UUID?
+    let content: () -> Content
+    
+    public var body: some View {
+        ZStack(alignment: .top) {
+            content()
+            if manager.shouldDisplay,
+               let title = manager.title {
+                HUD(title, symbol: manager.symbol)
+                    .onAppear {
+                        id = manager.id
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                            if id == manager.id {
+                                withAnimation {
+                                    manager.dismiss()
+                                }
+                            }
+                        }
+                    }
+            }
+        }
+    }
+}
+
+public class HUDManager: ObservableObject {
+    public static let shared = HUDManager()
+    
+    @Published internal var shouldDisplay = false
+    @Published internal var id = UUID()
+    @Published internal var title = ""
+    @Published internal var symbol: SFSymbol? = nil
+    
+    public func display(_ title: String, symbol: SFSymbol? = nil) {
+        if shouldDisplay {
+            dismiss()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                withAnimation {
+                    self.title = title
+                    self.symbol = symbol
+                    self.id = UUID()
+                    self.shouldDisplay = true
+                }
+            }
+        } else {
+            withAnimation {
+                self.title = title
+                self.symbol = symbol
+                self.id = UUID()
+                self.shouldDisplay = true
+            }
+        }
+    }
+    
+    public func dismiss() {
+        withAnimation {
+            shouldDisplay = false
+        }
+    }
+    
+    public func forceDismiss() {
+        shouldDisplay = false
+    }
 }
